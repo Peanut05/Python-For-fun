a62afce0a3ce3634828308ff3451d4aae4b8df48
krushna Gavali <krushnagawali744@gmail.com>
2023-01-17 18:08:51 +0530

    Create Graphics_1.py

---
+++ Graphics_1.py
@@ -0,0 +1,9 @@
+import turtle as tu
+
+roo = tu.Turtle()  # Turtle object
+wn = tu.Screen()  # Screen Object
+wn.bgcolor("black")  # Screen Bg color
+wn.title("Fractal Tree Pattern")
+roo.left(90)  # moving the turtle 90 degrees towards left
+roo.speed(30)  # setting the speed of the turtle
+
